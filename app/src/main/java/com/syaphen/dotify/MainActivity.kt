b853592ee4ce6ee1152b6c22eddd60b854e01881
Erik Huang <huangti@uw.edu>
2020-04-09 10:58:39 -0700

    Submit button

--- app/src/main/java/com/syaphen/dotify/MainActivity.kt
+++ app/src/main/java/com/syaphen/dotify/MainActivity.kt
@@ -2,11 +2,17 @@ package com.syaphen.dotify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun submitClicked(view: View) {
+        Log.i("syaphen","haha")
     }
 }
